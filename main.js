9d31ec52a80f5b7dd7a0c097ad1b388a07947a17
William Chu <willchu1733@gmail.com>
2024-06-17 16:28:49 +1200

    add HTML div/button and css styling

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // let firstNumber = a
 // let secondNumber = b
 // let sumOperator =
-// TEST
+
 let a = 253
 let b = 12.5
+let operator = '+'
+
 const add = (a, b) => a + b
 
 const subtract = (a, b) => a - b
@@ -12,7 +14,9 @@ const multiple = (a, b) => a * b
 
 const divide = (a, b) => a / b
 
-// const operate = () => {}
+function operate(a, b, operator) {
+  return a + operator + b
+}
 
 console.log(`Add ${a} + ${b} = ` + add(a, b))
 console.log(`Subtract ${a} - ${b} = ` + subtract(a, b))
