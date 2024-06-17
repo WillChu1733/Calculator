8367902509bf4ef9d5d630f45d20a39270923607
William Chu <willchu1733@gmail.com>
2024-06-17 14:48:16 +1200

    complete step 1 create basic functions and test

---
+++ main.js
@@ -0,0 +1,20 @@
+// let firstNumber = a
+// let secondNumber = b
+// let sumOperator =
+
+let a = 253
+let b = 12.5
+const add = (a, b) => a + b
+
+const subtract = (a, b) => a - b
+
+const multiple = (a, b) => a * b
+
+const divide = (a, b) => a / b
+
+// const operate = () => {}
+
+console.log(`Add ${a} + ${b} = ` + add(a, b))
+console.log(`Subtract ${a} - ${b} = ` + subtract(a, b))
+console.log(`multiply ${a} * ${b} = ` + multiple(a, b))
+console.log(`divide ${a} / ${b} = ` + divide(a, b))
